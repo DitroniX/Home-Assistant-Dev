52552196a3943f7b4926d84d5660a8c57e814943
DitroniXDev <colin@turnerpost.co.uk>
2025-12-17 15:19:44 +0000

    Update __init__.py
    Typo on dev tag

--- components/ATM90E36/__init__.py
+++ components/ATM90E36/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 
-CODEOWNERS = ["@circuitsetup"]
+CODEOWNERS = ["@DitroniXDev"]
 
 atm90e36_ns = cg.esphome_ns.namespace("atm90e36")
 ATM90E36Component = atm90e36_ns.class_("ATM90E36Component", cg.Component)
