ec1a694de912668bfee17ea41ebcae1ec95394a9
Dave Williams | DitroniX | G8PUO <github@ditronix.co.uk>
2026-06-13 11:07:09 +0100

    Update __init__.py

--- components/tca6408/__init__.py
+++ components/tca6408/__init__.py
@@ -1,6 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import i2c, gpio, pins
+from esphome.components import i2c
+import esphome.pins as pins
 from esphome.const import CONF_ID, CONF_ADDRESS, CONF_INTERRUPT_PIN
 
 DEPENDENCIES = ["i2c"]
